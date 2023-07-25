5bdc4acb9e087d1c5d4a8f7af9ea5b1f43b86c6b
Frezze <frezze334@gmail.com>
2023-07-25 13:54:57 +0200

    update

--- script.js
+++ script.js
@@ -37,3 +37,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+function randomcolor(){
+  document.getElementById("skill-html").style.background="linear-gradient(45deg, #73c8fbd4, #a805abb8)"
+  document.getElementById("skill-js").style.background="linear-gradient(45deg, #73ffb9e6, #105304d4)"
+  document.getElementById("skill-css").style.background="linear-gradient(45deg, #4d1313c4, #e31360e0)"
+  document.getElementById("skill-python").style.background="linear-gradient(45deg, #dde145e0, #d19713ed)"
+  document.getElementById("skill-wordpress").style.background="linear-gradient(45deg, #001637b3, #0095ff99)"
+  document.getElementById("skill-mc").style.background="linear-gradient(45deg, #80db30, #a9960f)"
+  document.getElementById("skill-mysql").style.background="linear-gradient(45deg, #fe5affed, #65053a)"
+  document.getElementById("skill-adobe").style.background="linear-gradient(45deg, #259b18d9, #60b19ed6)"
+}
+
+function original_colors() {
+
+}
